f09b0a473b98fad0ba723808d4813cbff3dce0e4
Ebtesam <ebte.almasarani1@gmail.com>
2026-09-10 14:25:34 +0200

    'tog bort raden med att jag har att göra'

--- main.py
+++ main.py
@@ -2,5 +2,3 @@ hero_name = input("Vem ska historien handla om? ")
 hero_age = int(input("Hur gammal är hen? "))
 print("Det här är historien om", hero_name)
 print(hero_name, "var", hero_age, "år gammal.")
-
-print("Du har att göra ....")
